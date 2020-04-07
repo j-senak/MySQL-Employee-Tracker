594df77c67d8b5d675030e55a713e9e50a50cdb5
JohnS <senakj2011@hotmail.com>
2020-04-06 22:18:10 -0400

    Added role and department tables

--- schema.sql
+++ schema.sql
@@ -7,9 +7,17 @@ CREATE TABLE employee (
   id INT AUTO_INCREMENT NOT NULL,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  role_id INT (30) REFERENCES role(id),
-  manager_id INT(30) REFERENCES employee(id),
+  role_id INT REFERENCES role(id),
+  manager_id INT REFERENCES employee(id),
   PRIMARY KEY (id)
 );
 
-SELECT * FROM employee;
+CREATE TABLE role (
+    title VARCHAR NULL,
+    salary DECIMAL(10,4) NULL,
+    department_id INT REFERENCES department(name)
+);
+
+CREATE TABLE department (
+    name VARCHAR(30) NOT NULL
+);
