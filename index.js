4a5aef58d63cc012abccce2c1661cfeb6f7ddcd3
JohnS <senakj2011@hotmail.com>
2020-04-06 23:58:30 -0400

    Added prompt and function

--- index.js
+++ index.js
@@ -14,7 +14,56 @@ var connection = mysql.createConnection({
   database: "employee_DB",
 });
 
-connection.connect(function(err) {
-    if (err) throw err;
-    runSearch();
-  });
+connection.connect(function (err) {
+  if (err) throw err;
+  runPrompts();
+});
+
+function runPrompts() {
+  inquirer
+    .prompt({
+      name: "start",
+      type: "list",
+      message: "What would you like to do?",
+      choices: [
+        "View All Employees",
+        "View All Employees By Department",
+        "View All Employees By Manager",
+        "Add Employee",
+        "Remove Employee",
+        "Update Employee Role",
+        "Update Employee Manager",
+      ],
+    })
+    .then(function (choice) {
+      switch (choice.start) {
+        case "View All Employees":
+          viewAll();
+          break;
+
+        case "View All Employees By Department":
+          ViewByDept();
+          break;
+
+        case "View All Employees By Manager":
+          viewByManager();
+          break;
+
+        case "Add Employee":
+          addEmployee();
+          break;
+
+        case "Remove Employee":
+          removeEmployee();
+          break;
+
+        case "Update Employee Role":
+          updateRole();
+          break;
+
+        case "Update Employee Manager":
+          updateManager();
+          break;
+      }
+    });
+}
